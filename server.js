1d7d30a30b8b5a5210f021d517f2e269811f72a7
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-06-27 17:49:21 -0700

    finished updates. Nexmo is trial version, so will only work for one specified number until premium version purchase.

--- server.js
+++ server.js
@@ -50,8 +50,18 @@ function add() {
             email: answer.email,
             phonenumber: answer.phonenumber
         };
-        fs.appendFile("database.json", JSON.stringify(obj), err => {
-            console.log(err)
+        fs.readFile("database.json", "utf-8", (err, data) => {
+            var _json = JSON.parse(data)
+            _json.push(obj)
+            console.log(_json)
+            fs.writeFile("database.json", JSON.stringify(_json), (err, data) => {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log(data)
+                }
+            })
         })
     }).then(() => {
         start()
@@ -59,38 +69,78 @@ function add() {
 }
 
 function end() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "list",
+            message: "Would you like to message your contact list?",
+            choices: ["Yes", "No"]
+        },
+    ]).then(answers => {
+        switch (answers.name) {
+            case "Yes":
+                contact();
+                break;
+            case "No":
+                console.log("Program has ended. Thank you.")
+        }
+    })
+}
+function contact() {
     fs.readFile("database.json", "utf8", (err, data) => {
         if (err) {
             return console.log(err)
         } else {
-            console.log(JSON.parse(JSON.stringify(data)))
+            inquirer.prompt({
+                name: "message",
+                type: "input",
+                message: "Please input a message you would like to send.",
+            }).then(_message => {
+                var _data = JSON.parse((data))
+                for (let i = 0; i < _data.length; i++) {
+                    //Email
+                    var transporter = nodemailer.createTransport({
+                        service: 'Gmail',
+                        auth: {
+                            user: process.env.EMAIL || 'cashforkeystacoma@gmail.com', // TODO: your gmail account
+                            pass: process.env.PASS || 'gemini253' // TODO: your gmail password
+                        }
+                    });
+                    let mailOptions = {
+                        from: 'cashforkeystacoma@gmail.com', // TODO: email sender
+                        to: _data[i].email, // TODO: email receiver
+                        subject: `A Message For ${_data[i].first_name} ${_data[i].last_name}`,
+                        text: _message.message
+                    };
+                    transporter.sendMail(mailOptions, (err, data) => {
+                        if (err) {
+                            return (err);
+                        }
+                        return log('Email sent!!!');
+                    });
+                    //Text
+                    const Nexmo = require("nexmo")
+                    const nexmo = new Nexmo({
+                        apiKey: process.env.APIKEY,
+                        apiSecret: process.env.SECRET
+                    }, { debug: true });
+                    const number = _data[i].phonenumber.length >= 7 ? false : "1" + _data[i].phonenumber;
+                    const message = `Hi ${_data[i].first_name} ${_data[i].last_name}, I would like to let you know that ${_message}`;
+                    nexmo.message.sendSms(
+                        "14256209722", number, message, { type: "unicode" }, (error, responseData) => {
+                            if (error) {
+                                console.log(error)
+                            } else {
+                                console.dir(responseData)
+                            }
+                        }
+                    )
+                }
+                console.log(`${_data.length} messages have been sent. This program will now shut off.`)
+            })
         }
-
     })
 }
-// Step 1
-// var transporter = nodemailer.createTransport({
-//     service: 'Gmail',
-//     auth: {
-//         user: process.env.EMAIL || 'cashforkeystacoma@gmail.com', // TODO: your gmail account
-//         pass: process.env.PASS || 'gemini253' // TODO: your gmail password
-//     }
-// });
 
-// // Step 2
-// let mailOptions = {
-//     from: 'adamcarlbirgenheier@gmail.com', // TODO: email sender
-//     to: 'adamcarlbirgenheier@gmail.com', // TODO: email receiver
-//     subject: 'Nodemailer - Test',
-//     text: 'Wooohooo it works!!'
-// };
-
-// // Step 3
-// transporter.sendMail(mailOptions, (err, data) => {
-//     if (err) {
-//         return log(err);
-//     }
-//     return log('Email sent!!!');
-// });
 
 start()
